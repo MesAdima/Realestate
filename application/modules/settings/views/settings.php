604b47d16e6f3b40f55ad242c9cadea4006c12eb
Adima Mesa <mesadima@gmail.com>
2015-01-22 16:33:05 +0300

    form open

---
+++ application/modules/settings/views/settings.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Real Estate</title>
+
+</head>
+<body>
+
+
+<div class="row">
+				<div class="col-md-12">
+				
+					<div class="block-flat">
+						<div class="header">							
+							<h3>Register House Type</h3>
+						</div>
+						<div class="content">
+						    <form class="form-horizontal group-border-dashed" action="#" novalidate>
+								<div class="form-group">
+									<label class="col-sm-3 control-label">Description</label>
+									<div class="col-sm-6">
+										<input type="text" data-mask="description" class="form-control"/>
+									</div>
+								</div>
+								<div class="form-group">
+									<label class="col-sm-3 control-label">House Type</label>
+									<div class="col-sm-6">
+										<input type="text" data-mask="type" class="form-control"/>
+									</div>
+								</div>
+								<div class="form-group">
+									<label class="col-sm-3 control-label">Multiple Houses</label>
+									<div class="col-sm-6">
+										<input type="checkbox" data-mask="multiple" class="form-control" value="yes"/> Yes <br>
+										<input type="checkbox" data-mask="multiple" class="form-control" value="no"/> No <br>
+									</div>
+								</div>
+								
+								<br>
+								<input type="submit" value="Submit">
+							</form>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+
+
+</body>
+</html>
